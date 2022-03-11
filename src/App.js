20271be4bbf54db45c227ce67783e8d6ab2f65a5
Uenderss <meajudabarto@gmail.com>
2022-03-11 16:23:10 -0300

    Implementacao do cabecalho

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header } from './header';
+import './styles/global.css'
+
+
+const App =()=>{
+    return <div>
+            
+            <Header/>
+            <h1>Piloto de Fuga</h1>
+        </div>;
+};
+
+export default App;
---
+++ src/header/index.js
@@ -0,0 +1,45 @@
+import { AiOutlineSearch, AiOutlineHeart } from "react-icons/ai";
+import { BsInstagram, BsPerson, BsChat, BsBookmark } from "react-icons/bs";
+import { FiSend } from "react-icons/fi";
+import { MdOutlineExplore } from "react-icons/md";
+import logo from "./logo.png";
+import "./style.css";
+
+export function Header() {
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="begin-menu">
+          <div className="img-instagram">
+            <BsInstagram />
+          </div>
+          <div className="coluna"></div>
+          <div className="logo-instagram">
+            <img src={logo} alt="Logo Instagram" />
+          </div>
+          <div className="img-paper">
+            <FiSend />
+          </div>
+        </div>
+        <div className="input-search">
+          
+          <div className="pesquisa">Pesquisa</div>
+        </div>
+        <div className="icones-menu">
+          <div className="img-paper">
+            <FiSend />
+          </div>
+          <div className="img-compass">
+            <MdOutlineExplore />
+          </div>
+          <div className="img-heart">
+            <AiOutlineHeart />
+          </div>
+          <div className="img-person">
+            <BsPerson />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
